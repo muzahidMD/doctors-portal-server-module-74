9d3c373da38e4aec0371ce619b66619b1208d894
Muzahidul Islam <mujahidul.core@gmail.com>
2022-05-20 21:24:33 +0600

    set booking API

--- index.js
+++ index.js
@@ -19,15 +19,36 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('bookings');
 
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
-        })
+        });
 
+        /**
+         * -----------------------
+         * API Naming Convention
+         * -----------------------
+         * app.get('/booking') // get all bookings in this collection, or get more then one or by filter
+         * app.get('/booking/:id') // get a specific booking
+         * app.post('/booking') // add a new booking
+         * app.patch('/booking/:id') // update a booking
+         * app.delete('/booking') // delete a booking
+        */
 
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient };
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            };
+            const result = bookingCollection.insertOne(booking);
+            return res.send({ success: true, result });
+        });
     }
     finally {
 
